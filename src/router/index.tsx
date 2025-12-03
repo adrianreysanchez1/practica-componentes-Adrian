06fc4bc65079eb668250a5dbffb0967ac2f03fda
Iván <ivan.corrales@wesovilabs.com>
2025-12-03 10:17:54 +0100

    Inicial commit

---
+++ src/router/index.tsx
@@ -0,0 +1,11 @@
+import DetalleAlumno from "@/pages/DetalleAlumno.tsx";
+import ListadoAlumnos from "@/pages/ListadoAlumnos.tsx";
+import PageNotFound from "@/pages/PageNotFound.tsx";
+import {createBrowserRouter} from "react-router-dom";
+
+
+
+export const router = createBrowserRouter([
+   // {path: "*", element: <PageNotFound />},
+
+]);
